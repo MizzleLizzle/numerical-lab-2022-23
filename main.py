047c901bf6216bbbefdcf2ebb97975e45e6704e6
Mikael Lux <mikael.lux@singular-it.de>
2022-11-17 00:27:15 +0100

    plotting

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from solvers import newtons_error_series
 from example_functions import f_1, f_1_prime
+import matplotlib.pyplot as plt
+
+
+def plot_errors(errors):
+    fig, ax = plt.subplots()  # Create a figure containing a single axes.
+    ax.set_yscale('log')
+    ax.plot(errors)
+    plt.show()
+
 
 def main():
-    print(newtons_error_series(f_1, f_1_prime, 0.8, max_iterations=10000, tolerance=0.001))
-
+    errors = newtons_error_series(
+        f_1, f_1_prime, 3.8, max_iterations=10000, tolerance=0.00001)
+    plot_errors(errors)
 
 
 if __name__ == '__main__':
