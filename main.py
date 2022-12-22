4b3346f6e1b93e3dcf6869b3d049ce5bd6cf289c
Mikael Lux <mikael.lux@singular-it.de>
2022-12-22 11:19:32 +0100

    fix fixed point iteration

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from solvers import newtons_error_series, secant_error_series, bisection_error_series, fixed_point_error_series
+from solvers import newtons_error_series, secant_error_series, bisection_error_series, fixed_point_error_series, fixed_point
 from example_functions import f_1, f_1_prime, g_1_b, g_1_a, g_2_a, g_2_b
 import matplotlib.pyplot as plt
 from sys import float_info
 
 
-def plot_errors(f, f_prime, g, x):
+def plot_errors(f, f_prime, g, tolerance):
     fig, ax = plt.subplots()
 
-    newtons_errors = newtons_error_series(f, f_prime, x, 10000, 0.0001)
-    secant_errors = secant_error_series(f, 0, 1, 10000, 0.0001)
-    bisection_errors = bisection_error_series(f, 0, 1, 10000, 0.0001)
+    newtons_errors = newtons_error_series(f, f_prime, 1, 10000, tolerance)
+    secant_errors = secant_error_series(f, 0, 1, 10000, tolerance)
+    bisection_errors = bisection_error_series(f, 0, 1, 10000, tolerance)
     try:
-        fixed_point_errors = fixed_point_error_series(g, x, 10000, 0.0001)
+        fixed_point_errors = fixed_point_error_series(g, 1, 10000, tolerance)
         ax.plot(range(len(fixed_point_errors)),
                 fixed_point_errors, label='Fixpunkt '+g.__name__)
     except:
@@ -31,7 +31,7 @@ def plot_errors(f, f_prime, g, x):
 
 
 def main():
-    plot_errors(f_1, f_1_prime, g_1_b, 0.5)
+    plot_errors(f_1, f_1_prime, g_1_b, 0.0001)
 
 
 if __name__ == '__main__':
--- solvers.py
+++ solvers.py
@@ -14,7 +14,7 @@ def newtons(f: Callable[[float], float], f_prime: Callable[[float], float], x: f
     raise NoRootFoundException
 
 
-def newtons_error_series(f: Callable[[float], float], f_prime: Callable[[float], float], x: float, max_iterations: int, tolerance: float) -> float:
+def newtons_error_series(f: Callable[[float], float], f_prime: Callable[[float], float], x: float, max_iterations: int, tolerance: float) -> list[float]:
     errors = [x]
     for _ in range(max_iterations):
         x = x - f(x)/f_prime(x)
@@ -34,8 +34,8 @@ def secant(f: Callable[[float], float], x_0: float, x_1: float, max_iterations: 
     raise NoRootFoundException
 
 
-def secant_error_series(f: Callable[[float], float], x_0: float, x_1: float, max_iterations: int, tolerance: float) -> float:
-    errors = [x_0, x_1]
+def secant_error_series(f: Callable[[float], float], x_0: float, x_1: float, max_iterations: int, tolerance: float) -> list[float]:
+    errors = [x_1]
     for _ in range(max_iterations):
         swap = x_1
         x_1 = x_1 - (f(x_1)*(x_1-x_0))/(f(x_1)-f(x_0))
@@ -63,13 +63,13 @@ def bisection(f: Callable[[float], float], a: float, b: float, max_iterations: i
     raise NoRootFoundException
 
 
-def bisection_error_series(f: Callable[[float], float], a: float, b: float, max_iterations: int, tolerance: float) -> float:
+def bisection_error_series(f: Callable[[float], float], a: float, b: float, max_iterations: int, tolerance: float) -> list[float]:
     def sign(y: float) -> int:
         if y >= 0:
             return 1
         return -1
 
-    errors = [a]
+    errors = [b]
     for _ in range(max_iterations):
         x = (a+b)/2
         errors.append(x)
@@ -85,16 +85,16 @@ def bisection_error_series(f: Callable[[float], float], a: float, b: float, max_
 def fixed_point(f: Callable[[float], float], x: float, max_iterations: int, tolerance: float) -> float:
     for _ in range(max_iterations):
         x = f(x)
-        if abs(f(x)) < tolerance:
+        if abs(f(x)-x) < tolerance:
             return x
     raise NoRootFoundException
 
 
-def fixed_point_error_series(f: Callable[[float], float], x: float, max_iterations: int, tolerance: float) -> float:
+def fixed_point_error_series(f: Callable[[float], float], x: float, max_iterations: int, tolerance: float) -> list[float]:
     errors = [x]
     for _ in range(max_iterations):
         x = f(x)
         errors.append(x)
-        if abs(f(x)) < tolerance:
+        if abs(f(x)-x) < tolerance:
             return [abs(x-y) for y in errors]
     raise NoRootFoundException
